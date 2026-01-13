71baf248d15bca64887586a0d51899ce09a57a62
Chinmoy <chinmoy.biswas@streebo.com>
2026-01-13 23:06:25 +0530

    fix: Update domain URL for Bandhu - Motors Test environment

--- src/services/axios-config/domain.js
+++ src/services/axios-config/domain.js
@@ -4,4 +4,8 @@
 //export const domainUrl = "http://localhost:5080/api/";
 
 //Test
-export const domainUrl = "https://frnmotorsbackend.onrender.com/api/";
+//export const domainUrl = "https://frnmotorsbackend.onrender.com/api/";
+
+
+//Bandhu - Motors Test
+export const domainUrl = "https://frnmotorsbackend-axh3.onrender.com/api/";
